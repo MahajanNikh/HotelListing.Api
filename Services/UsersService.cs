c510cb9e1afc9af5ad51fb3403d1f7c1e749f703
MahajanNikh <mahajan12647@gmail.com>
2026-03-14 18:32:38 +0530

    updated user service and authController for jwt token creation

--- Services/UsersService.cs
+++ Services/UsersService.cs
@@ -4,10 +4,14 @@ using HotelListing.Api.Data;
 using HotelListing.Api.DTOs.Auth;
 using HotelListing.Api.Results;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace HotelListing.Api.Services;
 
-public class UsersService(UserManager<ApplicationUser> userManager) : IUsersService
+public class UsersService(UserManager<ApplicationUser> userManager, IConfiguration configuration) : IUsersService
 {
     public async Task<Result<RegisteredUserDto>> RegisterAsync(RegisterUserDto registerUserDto)
     {
@@ -47,12 +51,48 @@ public class UsersService(UserManager<ApplicationUser> userManager) : IUsersServ
         {
             return Result<string>.Failure(new Error(ErrorCodes.BadRequest, "Invalid Credentials"));
         }
-        return Result<string>.Success("Login Successful.");
+
+        // Issue a token
+        var token = await GenerateToken(user);
+
+        return Result<string>.Success(token);
     }
 
-    //private async Task<string> GenerateToken(ApplicationUser user)
-    //{
-    //    //Set basic 
+    private async Task<string> GenerateToken(ApplicationUser user)
+    {
 
-    //}
+        // Set basic user claims
+        var claims = new List<Claim>
+        {
+            new(JwtRegisteredClaimNames.Sub, user.Id),
+            new(JwtRegisteredClaimNames.Email, user.Email),
+            new(JwtRegisteredClaimNames.Jti , Guid.NewGuid().ToString()),
+            new(JwtRegisteredClaimNames.Name, user.FullName)
+        };
+
+        // Set user roles claims
+
+        var roles = await userManager.GetRolesAsync(user);
+        var roleClaims = roles.Select(x => new Claim(ClaimTypes.Role, x)).ToList();
+
+        claims = claims.Union(roleClaims).ToList();
+
+        // Set JWT Key credentials
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:Key"]));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        // Create an encoded token
+        var token = new JwtSecurityToken(
+            issuer: configuration["JwtSettings:Issuer"],
+            audience: configuration["JwtSettings:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(Convert.ToInt32(configuration["JwtSettings:DurationInMinutes"])),
+            signingCredentials: credentials
+            );
+
+        // Return token value
+        return new JwtSecurityTokenHandler().WriteToken(token);
+
+
+    }
 }
